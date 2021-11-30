9a133f4fc20fc9a81ccb7562151ace5e6eae407f
Biglanchik <95071859+Biglanchik@users.noreply.github.com>
2021-12-01 00:00:26 +0300

    Update Program.cs

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -6,8 +6,8 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello");
+            for (int i=0 i<3 i++)
+            Console.WriteLine("Vanek");
         }
         
        string name = "dkhfsbjfdjs";
